df3ca641d9fc557c6cdf0d9662b8f455e2ca229d
SonCV <soncv97@gmail.com>
2022-01-11 09:27:18 +0700

    update bai 10

--- Week1/src/basic/Bai10.java
+++ Week1/src/basic/Bai10.java
@@ -5,7 +5,7 @@ public class Bai10 {
         if (n<2) return false;
         if (n==2 ) return true;
         if(n>2){
-            for (int i = 2; i <= n/2; i+=2) {
+            for (int i = 2; i <= n/2; i++) {
                 if( n % i == 0 ) return false;
             }
         }
@@ -24,21 +24,27 @@ public class Bai10 {
     }
     static boolean ktcs(int n){
         int[] arr = convertToArr(n);
-        int s = 0;
-        for (int i = 0; i <7; i++) {
-            s = (int) (s + (Math.pow(10, 6 - i)) * arr[i]);
-        }
         for (int i = 0; i < 7; i++) {
-            if (!ktnt(i)) return false;
+            if (ktnt(arr[i])==false) return false;
         }
         return true;
+    }
+    static boolean check(int n){
+        int reversed = 0;
+        while(n != 0) {
+            int digit = n % 10;
+            reversed = reversed * 10 + digit;
+            n /= 10;
+        }
+        if(ktnt(reversed)) return true;
+        return false;
     }
 
     public static void main(String[] args) {
         for (int i = 1000001; i < 9999999; i+=2) {
-            if(ktnt(i)){
-                if(ktcs(i)) System.out.println(i);
-            }
+            if(ktnt(i))
+                if(ktcs(i))
+                    if(check(i)) System.out.println(i);
         }
     }
 }
